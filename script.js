64c18cac04f87ebf2281df98ec1138ab8d132f99
MishaTarasenko <misha0106@gmail.com>
2023-06-20 13:18:24 +0300

    last

--- script.js
+++ script.js
@@ -13,22 +13,31 @@ switch(button.className){
       typeOfPizzas = button.textContent;
       PrintPizza();
       ChangeButtonStile(button);
+      SaveData();
       break;
    case "buyButtonText":
       BuyButtonClicked(button);
-      //saveData();
+      SaveData();
       break;
    case "plusButton":
       PlusButtonClicked(button);
-      //saveData();
+      SaveData();
       break;
    case "minusButton":
       MinusButtonClicked(button);
-      //saveData();
+      SaveData();
       break;
    case "cancelButton":
       CancelButtonClicked(button);
-      //saveData();
+      SaveData();
+      break;
+   case "clearButton":
+      DeleteOrderClicked();
+      SaveData();
+      break;
+   case "confirm":
+      OrderButtonClicked();
+      SaveData();
       break;
    }
 });
@@ -77,7 +86,7 @@ if(typeOfPizzas == "Усі"){
             </section>
             <section class="weight">
                <img class="weightLabel" src="weight.svg" alt="weight">
-               <article class="text">${pizzasInformation[i].bigSize.weight}</article>
+               <article class="text weight">${pizzasInformation[i].bigSize.weight}</article>
             </section>
             <section class="price">
                <article class="textPrice">${pizzasInformation[i].bigSize.price}</article>
@@ -89,6 +98,27 @@ if(typeOfPizzas == "Усі"){
          </section>
       </section>
       `;
+      if(pizzasInformation[i].is_new == true){
+         let labelNew = document.createElement("section");
+         labelNew.setAttribute("class", "label new");
+
+         let labelText = document.createElement("article");
+         labelText.setAttribute("class", "labelText");
+         labelText.textContent = "Нова";
+         labelNew.appendChild(labelText);
+
+         pizza.appendChild(labelNew);
+      }else if(pizzasInformation[i].is_popular == true){
+         let labelNew = document.createElement("section");
+         labelNew.setAttribute("class", "label popular");
+
+         let labelText = document.createElement("article");
+         labelText.setAttribute("class", "labelText");
+         labelText.textContent = "Популярна";
+         labelNew.appendChild(labelText);
+
+         pizza.appendChild(labelNew);
+      }
       pizzas.appendChild(pizza);
    }
 }else{
@@ -133,7 +163,7 @@ if(typeOfPizzas == "Усі"){
                </section>
                <section class="weight">
                   <img class="weightLabel" src="weight.svg" alt="weight">
-                  <article class="text">${pizzasInformation[i].bigSize.weight}</article>
+                  <article class="text weight">${pizzasInformation[i].bigSize.weight}</article>
                </section>
                <section class="price">
                   <article class="textPrice">${pizzasInformation[i].bigSize.price}</article>
@@ -145,6 +175,27 @@ if(typeOfPizzas == "Усі"){
             </section>
          </section>
          `;
+         if(pizzasInformation[i].is_new == true){
+            let labelNew = document.createElement("section");
+            labelNew.setAttribute("class", "label new");
+   
+            let labelText = document.createElement("article");
+            labelText.setAttribute("class", "labelText");
+            labelText.textContent = "Нова";
+            labelNew.appendChild(labelText);
+   
+            pizza.appendChild(labelNew);
+         }else if(pizzasInformation[i].is_popular == true){
+            let labelNew = document.createElement("section");
+            labelNew.setAttribute("class", "label popular");
+   
+            let labelText = document.createElement("article");
+            labelText.setAttribute("class", "labelText");
+            labelText.textContent = "Популярна";
+            labelNew.appendChild(labelText);
+   
+            pizza.appendChild(labelNew);
+         }
          pizzas.appendChild(pizza);
       }
    }
@@ -205,7 +256,7 @@ function BuyButtonClicked(button){
                         </section>
                         <section class="weight">
                            <img class="weightLabel" src="weight.svg" alt="weight">
-                           <article class="text">${button.parentNode.parentNode.querySelector(".weight").querySelector(".text").textContent}</article>
+                           <article class="text weight">${button.parentNode.parentNode.querySelector(".weight").querySelector(".text").textContent}</article>
                         </section>
                      </section>
                      <section class="orderButtonGroup">
@@ -250,18 +301,68 @@ function BuyButtonClicked(button){
 }
 
 function PlusButtonClicked(button){
+   let pizzaOrder = button.parentNode.parentNode.parentNode;
+   let name = pizzaOrder.querySelector(".orderPizzaName").textContent;
+   button.parentNode.querySelector(".amountText").textContent++;
 
+   for(let i = 0; i < pizzasInformation.length; i++){
+      if(name.indexOf(pizzasInformation[i].pizzaName) != -1){
+         if(name.indexOf("(Мала)") != -1){
+            button.parentNode.parentNode.querySelector(".orderPrice.money").textContent = parseInt(button.parentNode.parentNode.querySelector(".orderPrice.money").textContent) + pizzasInformation[i].smallSize.price;
+            document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) + pizzasInformation[i].smallSize.price;
+         }
+         else{
+            button.parentNode.parentNode.querySelector(".orderPrice.money").textContent = parseInt(button.parentNode.parentNode.querySelector(".orderPrice.money").textContent) + pizzasInformation[i].bigSize.price;
+            document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) + pizzasInformation[i].bigSize.price;
+         }
+         document.querySelector(".ordersTop").querySelector(".amount").textContent++;
+      }
+   }
 }
 
 function MinusButtonClicked(button){
-
+   let pizzaOrder = button.parentNode.parentNode.parentNode;
+   let name = pizzaOrder.querySelector(".orderPizzaName").textContent;
+   if(button.parentNode.querySelector(".amountText").textContent - 1 != 0){
+      button.parentNode.querySelector(".amountText").textContent--;
+      for(let i = 0; i < pizzasInformation.length; i++){
+         if(name.indexOf(pizzasInformation[i].pizzaName) != -1){
+            if(name.indexOf("(Мала)") != -1){
+               button.parentNode.parentNode.querySelector(".orderPrice.money").textContent = parseInt(button.parentNode.parentNode.querySelector(".orderPrice.money").textContent) - pizzasInformation[i].smallSize.price;
+               document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) - pizzasInformation[i].smallSize.price;
+            }
+            else{
+               button.parentNode.parentNode.querySelector(".orderPrice.money").textContent = parseInt(button.parentNode.parentNode.querySelector(".orderPrice.money").textContent) - pizzasInformation[i].bigSize.price;
+               document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) - pizzasInformation[i].bigSize.price;
+            }
+            document.querySelector(".ordersTop").querySelector(".amount").textContent--;
+         }
+      }
+   }else{
+      document.querySelector(".orders").removeChild(button.parentNode.parentNode.parentNode.parentNode);
+   }
 }
 
 function CancelButtonClicked(button){
-
+   document.querySelector(".ordersTop").querySelector(".amount").textContent = parseInt(document.querySelector(".ordersTop").querySelector(".amount").textContent) - parseInt(button.parentNode.querySelector(".amountText").textContent);
+   document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) - parseInt(button.parentNode.parentNode.querySelector(".orderPrice.money").textContent);
+   document.querySelector(".orders").removeChild(button.parentNode.parentNode.parentNode);
 }
 
+function DeleteOrderClicked(){
+   document.querySelector(".ordersTop").querySelector(".amount").textContent = 0;
+   document.querySelector(".sumInUAN.money").textContent = 0;
+   let order = Array.from(document.querySelectorAll(".order"));
+   let n = order.length;
+   for(let i = 0; i < n; i++){
+      document.querySelector(".orders").removeChild(order[i]);
+   }
+}
 
+function OrderButtonClicked(){
+   DeleteOrderClicked();
+   alert("Ваш заказ успішно прийнятий!!!\n Зачекайте на підтверджуюче повідомлення");
+}
 
 window.onscroll = function() {
    var scrolled = document.documentElement.scrollTop;
@@ -271,5 +372,116 @@ window.onscroll = function() {
      document.querySelector('.top').style.opacity = '1';
    };
  };
+
+ function SaveData() {
+   const data = getIncomeRows().map((row) => {
+      return {
+         name: row.querySelector(".orderPizzaName").textContent.trim(),
+         amount: row.querySelector(".amountText").textContent,
+         weight: row.querySelector(".text.weight").textContent,
+         price: row.querySelector(".orderPrice").textContent,
+      };
+   });
+ 
+   localStorage.setItem("orders", JSON.stringify(data));
+ }
+ 
+ function getIncomeRows() {
+   return Array.from(document.querySelectorAll(".orders .order"));
+ }
+
+ function getOrders(){
+   if (localStorage.getItem("products") == "[]") {
+
+   }else{
+      let orders = JSON.parse(localStorage.getItem("orders"));
+      orders.forEach(function (order) {
+      let newOrder = document.createElement("section");
+      newOrder.setAttribute("class", "order");
+      newOrder.innerHTML=`
+         <section class="orderDiscription">
+            <article class="orderPizzaName">
+               ${order.name}
+            </article>
+            <section class="orderPizzaCharacteristic">
+               <section class="diameter">
+               </section>
+               <section class="weight">
+               </section>
+            </section>
+            <section class="orderButtonGroup">
+               <section class="orderPriceSection">
+                  <article class="orderPrice money">
+                     ${order.price}
+                  </article>
+                  <article class="orderPrice">
+                     грн
+                  </article>
+               </section>
+               <section class="orderAmount">
+                  <button class="minusButton">–</button>
+                  <article class="amountText">${order.amount}</article>
+                  <button class="plusButton">+</button>
+               </section>
+               <button class="cancelButton">&#10006;</button>
+            </section>
+         </section>
+         <section class="orderImg">
+         </section>
+         `;
+   
+         let orderImg = document.createElement("img");
+         orderImg.setAttribute("class", "orderPizzaImg");
+         for(let i = 0; i < pizzasInformation.length; i++){
+            if(order.name.indexOf(pizzasInformation[i].pizzaName) != -1){
+               orderImg.setAttribute("src", pizzasInformation[i].pizzaImg);
+               break;
+            }
+         }
+         orderImg.setAttribute("alt", "картинка піци");
+         newOrder.querySelector(".orderImg").appendChild(orderImg);
+   
+         let weightLabel = document.createElement("img");
+         weightLabel.setAttribute("class", "diameterLabel");
+         weightLabel.setAttribute("src", "weight.svg");
+         weightLabel.setAttribute("alt", "diameter");
+         newOrder.querySelector(".weight").appendChild(weightLabel);
+   
+         let textWeight = document.createElement("article");
+         textWeight.setAttribute("class", "text weight");
+         textWeight.textContent = order.weight;
+         newOrder.querySelector(".weight").appendChild(textWeight);
+   
+         if(order.name.indexOf("(Мала)")){
+            let diameterLabel = document.createElement("img");
+            diameterLabel.setAttribute("class", "diameterLabel");
+            diameterLabel.setAttribute("src", "size-icon.svg");
+            diameterLabel.setAttribute("alt", "diameter");
+            newOrder.querySelector(".diameter").appendChild(diameterLabel);
+   
+            let textDiameter = document.createElement("article");
+            textDiameter.setAttribute("class", "text");
+            textDiameter.textContent = 30;
+            newOrder.querySelector(".diameter").appendChild(textDiameter);
+         }else{
+            let diameterLabel = document.createElement("img");
+            diameterLabel.setAttribute("class", "diameterLabel");
+            diameterLabel.setAttribute("src", "size-icon.svg");
+            diameterLabel.setAttribute("alt", "diameter");
+            newOrder.querySelector(".diameter").appendChild(diameterLabel);
+   
+            let textDiameter = document.createElement("article");
+            textDiameter.setAttribute("class", "text");
+            textDiameter.textContent = 40;
+            newOrder.querySelector(".diameter").appendChild(textDiameter);
+         }
+         document.querySelector(".orders").appendChild(newOrder);
+         document.querySelector(".sumInUAN.money").textContent = parseInt(document.querySelector(".sumInUAN.money").textContent) + parseInt(order.price);
+         document.querySelector(".ordersTop").querySelector(".amount").textContent = parseInt(document.querySelector(".ordersTop").querySelector(".amount").textContent) + parseInt(order.amount);
+      });
+   }
+}
+
+document.addEventListener("DOMContentLoaded", getOrders());
 
 
